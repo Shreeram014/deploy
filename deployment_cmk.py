b7bc18db3125161dca615d038e4d2b45fe322113
Shriram Kulkarni <92530829+Shreeram014@users.noreply.github.com>
2023-07-16 17:28:34 +0530

    Update deployment_cmk.py

--- deployment_cmk.py
+++ deployment_cmk.py
@@ -70,7 +70,7 @@ def satisfaction_prediction_svm(type_of_travel, class_, Flight_distance, Infligh
     
     return prediction
 
-result = 0
+
 st.button('Prediction')
 result = satisfaction_prediction_svm(type_of_travel, class_, Flight_distance, Inflight_wifi_service, Online_boarding, Seat_comfort, Inflight_entertainment, On_board_service, Leg_room_service)
 st.success('predict'.format(result))
